756a9123abbb56b51c4c6fb30c0e89b4c770768f
frivas12 <panchy.rivas@gmail.com>
2026-02-02 13:34:07 -0500

    - added clock_out to S70 - build project when run

--- 70-0059/proj/Firmware/src/system/boards/init.c
+++ 70-0059/proj/Firmware/src/system/boards/init.c
@@ -7,38 +7,37 @@
  digraph G {
  node [shape=doublecircle]; "usb host task"; "error task";
  node [shape=ellipse];
- "init clocks and irq" -> "enable caches" -> "disable TCM" -> "start peripherals";
- "start peripherals" -> "reset CPLD" -> "setup SPI" -> "config USB Host";
- "config USB Host" -> "initialize slots" -> "config error thread" -> "return";
- "config USB Host" -> "usb host task";
- "config error thread" -> "error task";
+ "init clocks and irq" -> "enable caches" -> "disable TCM" -> "start
+ peripherals"; "start peripherals" -> "reset CPLD" -> "setup SPI" -> "config USB
+ Host"; "config USB Host" -> "initialize slots" -> "config error thread" ->
+ "return"; "config USB Host" -> "usb host task"; "config error thread" -> "error
+ task";
  }
  * @enddot
  */
 #include <asf.h>
-#include "board.h"
-#include "Debugging.h"
-#include <pins.h>
-#include "usb_slave.h"
-#include "user_spi.h"
 #include <cpld.h>
+#include <pins.h>
+
 #include "../drivers/supervisor/supervisor.h"
-#include "usb_host.h"
-#include "rt1064_spi_bridge.h"
 #include "25lc1024.h"
-#include "soft_i2c.h"
+#include "Debugging.h"
+#include "board.h"
+#include "conf_clock.h"
 #include "delay.h"
-#include "user_adc.h"
-#include "user_usart.h"
-#include "usr_interrupt.h"
+#include "efs.h"
 #include "helper.h"
 #include "lut_manager.h"
-#include "efs.h"
-#include "service-inits.h"
-
-
-#include "conf_clock.h"
 #include "pmc.h"
+#include "rt1064_spi_bridge.h"
+#include "service-inits.h"
+#include "soft_i2c.h"
+#include "usb_host.h"
+#include "usb_slave.h"
+#include "user_adc.h"
+#include "user_spi.h"
+#include "user_usart.h"
+#include "usr_interrupt.h"
 
 /****************************************************************************
  * Function Prototypes
@@ -60,208 +59,202 @@ extern char _itcm_lma, _sitcm, _eitcm;
 /** \brief  TCM memory enable
  * The function enables TCM memories
  */
-static inline void tcm_enable(void)
-{
+static inline void tcm_enable(void) {
+    __DSB();
+    __ISB();
 
-	__DSB();
-	__ISB();
+    SCB->ITCMCR =
+        (SCB_ITCMCR_EN_Msk | SCB_ITCMCR_RMW_Msk | SCB_ITCMCR_RETEN_Msk);
+    SCB->DTCMCR =
+        (SCB_DTCMCR_EN_Msk | SCB_DTCMCR_RMW_Msk | SCB_DTCMCR_RETEN_Msk);
 
-	SCB->ITCMCR = (SCB_ITCMCR_EN_Msk | SCB_ITCMCR_RMW_Msk | SCB_ITCMCR_RETEN_Msk);
-	SCB->DTCMCR = ( SCB_DTCMCR_EN_Msk | SCB_DTCMCR_RMW_Msk | SCB_DTCMCR_RETEN_Msk);
-
-	__DSB();
-	__ISB();
+    __DSB();
+    __ISB();
 }
 #else
 /** \brief  TCM memory Disable
 
  The function enables TCM memories
  */
-static inline void tcm_disable(void)
-{
-
-	__DSB();
-	__ISB();
-	SCB->ITCMCR &= ~(uint32_t) (1UL);
-	SCB->DTCMCR &= ~(uint32_t) SCB_DTCMCR_EN_Msk;
-	__DSB();
-	__ISB();
+static inline void tcm_disable(void) {
+    __DSB();
+    __ISB();
+    SCB->ITCMCR &= ~(uint32_t)(1UL);
+    SCB->DTCMCR &= ~(uint32_t)SCB_DTCMCR_EN_Msk;
+    __DSB();
+    __ISB();
 }
 #endif
 
-static void flash_Init(void)
-{
-        uint32_t ul_rc;
-        ul_rc = flash_init(FLASH_ACCESS_MODE_128, 6);
-        if (ul_rc != FLASH_RC_OK)
-        {
-        	debug_print("ERROR: flash_Init failed");
-        }
+static void flash_Init(void) {
+    uint32_t ul_rc;
+    ul_rc = flash_init(FLASH_ACCESS_MODE_128, 6);
+    if (ul_rc != FLASH_RC_OK) {
+        debug_print("ERROR: flash_Init failed");
+    }
 }
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-static void reset_firmware_load_count(void)
-{
-        uint32_t StatusFlg;
-        uint32_t BootCount[1];
-        StatusFlg = flash_read_user_signature(BootCount, 1);
-        BootCount[0] = 0;
-        StatusFlg = flash_erase_user_signature();
-        delay_ms(10);
-        StatusFlg = flash_write_user_signature(BootCount, 1);
+static void reset_firmware_load_count(void) {
+    uint32_t StatusFlg;
+    uint32_t BootCount[1];
+    StatusFlg = flash_read_user_signature(BootCount, 1);
+    BootCount[0] = 0;
+    StatusFlg = flash_erase_user_signature();
+    delay_ms(10);
+    StatusFlg = flash_write_user_signature(BootCount, 1);
 }
 #pragma GCC diagnostic pop
 
-static void program_fuses(void)
-{
-	#define BOOTLOADER_FIRST_SECTOR 0x00400000
-	#define BOOTLOADER_LAST_SECTOR 0x0041C000
-	#define BOOTLOADER_REGIONS 8
-	uint32_t locked_regions;
-	locked_regions = flash_is_locked(BOOTLOADER_FIRST_SECTOR, BOOTLOADER_LAST_SECTOR);
+static void program_fuses(void) {
+#define BOOTLOADER_FIRST_SECTOR 0x00400000
+#define BOOTLOADER_LAST_SECTOR 0x0041C000
+#define BOOTLOADER_REGIONS 8
+    uint32_t locked_regions;
+    locked_regions =
+        flash_is_locked(BOOTLOADER_FIRST_SECTOR, BOOTLOADER_LAST_SECTOR);
 
-	if(locked_regions <BOOTLOADER_REGIONS)
-		flash_lock(BOOTLOADER_FIRST_SECTOR, BOOTLOADER_LAST_SECTOR, 0, 0);
+    if (locked_regions < BOOTLOADER_REGIONS)
+        flash_lock(BOOTLOADER_FIRST_SECTOR, BOOTLOADER_LAST_SECTOR, 0, 0);
 }
 
-static void use_mainclk_before_sysclk_init(void)
-{
-#if (CONFIG_SYSCLK_SOURCE == SYSCLK_SRC_PLLACK || CONFIG_SYSCLK_SOURCE == SYSCLK_SRC_UPLLCK)
-	if (PMC_MCKR_CSS(PMC->PMC_MCKR) != PMC_MCKR_CSS_MAIN_CLK)
-	{
-		// See https://forum.microchip.com/s/topic/a5C3l0000003kefEAA/t390550
-		if (pmc_switch_mck_to_mainck(CONFIG_SYSCLK_PRES) != 0) {
-			restart_board();
-		}
-	}
-	pmc_disable_pllack();
-	pmc_disable_upll_clock();
-	delay_ms(2);
+static void use_mainclk_before_sysclk_init(void) {
+#if (CONFIG_SYSCLK_SOURCE == SYSCLK_SRC_PLLACK || \
+     CONFIG_SYSCLK_SOURCE == SYSCLK_SRC_UPLLCK)
+    if (PMC_MCKR_CSS(PMC->PMC_MCKR) != PMC_MCKR_CSS_MAIN_CLK) {
+        // See https://forum.microchip.com/s/topic/a5C3l0000003kefEAA/t390550
+        if (pmc_switch_mck_to_mainck(CONFIG_SYSCLK_PRES) != 0) {
+            restart_board();
+        }
+    }
+    pmc_disable_pllack();
+    pmc_disable_upll_clock();
+    delay_ms(2);
 #endif
 }
-
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
 /**
- * @brief Initialize the processor details, the peripherals, then board and slot details
+ * @brief Initialize the processor details, the peripherals, then board and slot
+ * details
  *
  */
 
-void system_init(void)
-{
-	irq_initialize_vectors();
-	cpu_irq_enable();
-	use_mainclk_before_sysclk_init();
-	sysclk_init();
-	delay_ms(50);
+void system_init(void) {
+    irq_initialize_vectors();
+    cpu_irq_enable();
+    use_mainclk_before_sysclk_init();
+    sysclk_init();
+    delay_ms(50);
 
-	// Disable the watchdog */
-//	WDT->WDT_MR = WDT_MR_WDDIS | WDT_MR_WDDBGHLT;
+    // Disable the watchdog */
+    //	WDT->WDT_MR = WDT_MR_WDDIS | WDT_MR_WDDBGHLT;
 
 #ifdef CONF_BOARD_ENABLE_CACHE_AT_INIT
-	/* Enabling the Cache */
-	SCB_EnableICache();
-	SCB_EnableDCache();
+    /* Enabling the Cache */
+    SCB_EnableICache();
+    SCB_EnableDCache();
 #endif
 
 #ifdef CONF_BOARD_ENABLE_TCM_AT_INIT
-	/* TCM Configuration */
-	EFC->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB
-			| EEFC_FCR_FARG(8));
-	EFC->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_SGPB
-			| EEFC_FCR_FARG(7));
-	tcm_enable();
+    /* TCM Configuration */
+    EFC->EEFC_FCR =
+        (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(8));
+    EFC->EEFC_FCR =
+        (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_SGPB | EEFC_FCR_FARG(7));
+    tcm_enable();
 #if defined(__GNUC__)
-	volatile char *dst = &_sitcm;
-	volatile char *src = &_itcm_lma;
-	/* copy code_TCM from flash to ITCM */
-	while(dst < &_eitcm)
-	{
-		*dst++ = *src++;
-	}
+    volatile char* dst = &_sitcm;
+    volatile char* src = &_itcm_lma;
+    /* copy code_TCM from flash to ITCM */
+    while (dst < &_eitcm) {
+        *dst++ = *src++;
+    }
 #endif
 #else
-	/* TCM Configuration */
-	EFC->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(8));
-	EFC->EEFC_FCR = (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(7));
+    /* TCM Configuration */
+    EFC->EEFC_FCR =
+        (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(8));
+    EFC->EEFC_FCR =
+        (EEFC_FCR_FKEY_PASSWD | EEFC_FCR_FCMD_CGPB | EEFC_FCR_FARG(7));
 
-	tcm_disable();
+    tcm_disable();
 #endif
 
-	/* Initialize IOPORTs */
-	ioport_init();
+    /* Initialize IOPORTs */
+    ioport_init();
 
-	//Following three functions for Bootloader
+    // Following three functions for Bootloader
     flash_Init();
     reset_firmware_load_count();
     program_fuses();
 
-	/* PB5 function selected not TDO/TRACESWO*/
-	/* PB12 function select not ERASE*/
-	Matrix *p_matrix = MATRIX;
-	p_matrix->CCFG_SYSIO = 0x20401010;
+    /* PB5 function selected not TDO/TRACESWO*/
+    /* PB12 function select not ERASE*/
+    Matrix* p_matrix = MATRIX;
+    p_matrix->CCFG_SYSIO = 0x20401010;
 
 #if SYSTEM_VIEW_ENABLE
-	SEGGER_SYSVIEW_Conf();
-	debug_print("System boot was successful\r\n");
-	debug_print("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+    SEGGER_SYSVIEW_Conf();
+    debug_print("System boot was successful\r\n");
+    debug_print("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
 
 #endif
 
 #if DEBUG_PID_JSCOPE
-	JS_RTT_Channel = 1;
-	SEGGER_RTT_ConfigUpBuffer(JS_RTT_Channel, "JScope_I4i4", &JS_RTT_UpBuffer[0], sizeof(JS_RTT_UpBuffer), SEGGER_RTT_MODE_NO_BLOCK_SKIP);
+    JS_RTT_Channel = 1;
+    SEGGER_RTT_ConfigUpBuffer(JS_RTT_Channel, "JScope_I4i4",
+                              &JS_RTT_UpBuffer[0], sizeof(JS_RTT_UpBuffer),
+                              SEGGER_RTT_MODE_NO_BLOCK_SKIP);
 
 #endif
 
-	user_spi_init();
-	init_interrupts();
+    user_spi_init();
+    init_interrupts();
 
-	//TODO fix the case for hexapod needing to power up specially
-	set_pin_as_output(PIN_POWER_EN, 1);
+    // TODO fix the case for hexapod needing to power up specially
+    set_pin_as_output(PIN_POWER_EN, 1);
 
-	cpld_init();
+    cpld_init();
 
-	soft_i2c_init();
-	init_adc();
-	uart0_init();
+    soft_i2c_init();
+    init_adc();
+    uart0_init();
 
-	// Initialize slot synchronization objects to prevent system hang (even when programming cpld).
-	init_slots_synchronization();
+    // Initialize slot synchronization objects to prevent system hang (even when
+    // programming cpld).
+    init_slots_synchronization();
 
-	// Initialize supervisor with the cpld status.
-	// This is needed to periodically reset the WDT.
-	supervisor_init(cpld_programmed);
+    // Initialize supervisor with the cpld status.
+    // This is needed to periodically reset the WDT.
+    supervisor_init(cpld_programmed);
 
-	/*Only continue if the CPLD has been programmed, else the usb slave is running to load the
-	 * CPLD with software*/
-	if (cpld_programmed == true)
-	{
-		efs_init();
-		board_init();
+    /*Only continue if the CPLD has been programmed, else the usb slave is
+     * running to load the CPLD with software*/
+    if (cpld_programmed == true) {
+        efs_init();
+        board_init();
         hid_mapping_service_init();
         itc_service_init();
-		if (board_type < MCM_41_0117_RT1064)
-		{
-			usb_host_init();
-		}
-		else
-		{
-			rt1064_spi_bridge_init();
-		}
-	}
-	delay_ms(100);
-	usb_slave_init();
+        if (0)  //_type < MCM_41_0117_RT1064)
+        {
+            usb_host_init();
+        } else {
+            rt1064_spi_bridge_init();
+        }
+    }
+    delay_ms(100);
+    usb_slave_init();
 
 #if 1
-	/* Enable the watchdog, need to reload in the supervisor task */
-	uint32_t wdt_temp = WDT->WDT_MR;
-	Clr_bits(wdt_temp, WDT_MR_WDDIS);
-	Set_bits(wdt_temp, WDT_MR_WDDBGHLT);/* WDT stops in debug state. */
-	WDT->WDT_MR = wdt_temp;
+    /* Enable the watchdog, need to reload in the supervisor task */
+    uint32_t wdt_temp = WDT->WDT_MR;
+    Clr_bits(wdt_temp, WDT_MR_WDDIS);
+    Set_bits(wdt_temp, WDT_MR_WDDBGHLT); /* WDT stops in debug state. */
+    WDT->WDT_MR = wdt_temp;
 #endif
 }
--- 70-0059/proj/Firmware/src/system/drivers/usb_host/rt1064_spi_bridge.c
+++ 70-0059/proj/Firmware/src/system/drivers/usb_host/rt1064_spi_bridge.c
@@ -2,45 +2,42 @@
 
 #include "rt1064_spi_bridge.h"
 
-#include "Debugging.h"
-#include "sys_task.h"
-#include "user_spi.h"
-
-#include "FreeRTOS.h"
-#include "task.h"
-
 #include <string.h>
+
+#include "Debugging.h"
+#include "FreeRTOS.h"
+#include "sys_task.h"
+#include "task.h"
+#include "user_spi.h"
 
 #define SPI_BRIDGE_MAX_DEVICES (4U)
 #define SPI_BRIDGE_CDC_ENDPOINT_INDEX (SPI_BRIDGE_MAX_DEVICES + 1U)
 #define SPI_BRIDGE_MAX_LOGICAL_ENDPOINTS (SPI_BRIDGE_MAX_DEVICES + 2U)
 
-#define SPI_BRIDGE_CMD_OP_MASK  (0xC0U)
-#define SPI_BRIDGE_CMD_EN_MASK  (0x3FU)
+#define SPI_BRIDGE_CMD_OP_MASK (0xC0U)
+#define SPI_BRIDGE_CMD_EN_MASK (0x3FU)
 #define SPI_BRIDGE_CMD_OP_SHIFT (6U)
 
-#define SPI_BRIDGE_HEADER_DIRTY_MASK   (0x01U)
-#define SPI_BRIDGE_HEADER_TYPE_MASK    (0x02U)
-#define SPI_BRIDGE_HEADER_LENGTH_MASK  (0xFCU)
+#define SPI_BRIDGE_HEADER_DIRTY_MASK (0x01U)
+#define SPI_BRIDGE_HEADER_TYPE_MASK (0x02U)
+#define SPI_BRIDGE_HEADER_LENGTH_MASK (0xFCU)
 #define SPI_BRIDGE_HEADER_LENGTH_SHIFT (2U)
 
 #define SPI_BRIDGE_MAX_PAYLOAD_LENGTH (63U)
-#define SPI_BRIDGE_BLOCK_SIZE         (1U + SPI_BRIDGE_MAX_PAYLOAD_LENGTH + 2U)
+#define SPI_BRIDGE_BLOCK_SIZE (1U + SPI_BRIDGE_MAX_PAYLOAD_LENGTH + 2U)
 
 #define SPI_BRIDGE_CRC_POLY (0x1021U)
 #define SPI_BRIDGE_CRC_INIT (0xFFFFU)
 
 #define SPI_BRIDGE_POLL_PERIOD_MS (10U)
 
-typedef enum
-{
+typedef enum {
     kSpiBridgeCommandReadHeader = 0U,
-    kSpiBridgeCommandReadBlock  = 1U,
+    kSpiBridgeCommandReadBlock = 1U,
     kSpiBridgeCommandWriteBlock = 2U,
 } spi_bridge_command_t;
 
-typedef struct
-{
+typedef struct {
     uint8_t header;
     uint8_t payload[SPI_BRIDGE_MAX_PAYLOAD_LENGTH];
     uint16_t crc;
@@ -52,73 +49,61 @@ static spi_bridge_block_t s_cdc_in_block;
 static uint8_t s_connected_table[SPI_BRIDGE_MAX_DEVICES + 1U];
 static bool s_endpoint_active[SPI_BRIDGE_MAX_LOGICAL_ENDPOINTS];
 
-static uint8_t spi_bridge_extract_length(uint8_t header)
-{
-    return (uint8_t)((header & SPI_BRIDGE_HEADER_LENGTH_MASK) >> SPI_BRIDGE_HEADER_LENGTH_SHIFT);
+static uint8_t spi_bridge_extract_length(uint8_t header) {
+    return (uint8_t)((header & SPI_BRIDGE_HEADER_LENGTH_MASK) >>
+                     SPI_BRIDGE_HEADER_LENGTH_SHIFT);
 }
 
-static uint16_t spi_bridge_crc_update(uint16_t crc, uint8_t data)
-{
+static uint16_t spi_bridge_crc_update(uint16_t crc, uint8_t data) {
     crc ^= (uint16_t)data << 8U;
-    for (uint8_t i = 0U; i < 8U; ++i)
-    {
-        if ((crc & 0x8000U) != 0U)
-        {
+    for (uint8_t i = 0U; i < 8U; ++i) {
+        if ((crc & 0x8000U) != 0U) {
             crc = (uint16_t)((crc << 1U) ^ SPI_BRIDGE_CRC_POLY);
-        }
-        else
-        {
+        } else {
             crc <<= 1U;
         }
     }
     return crc;
 }
 
-static uint16_t spi_bridge_compute_crc(const uint8_t *raw)
-{
+static uint16_t spi_bridge_compute_crc(const uint8_t* raw) {
     uint8_t length = spi_bridge_extract_length(raw[0]);
-    uint16_t crc   = SPI_BRIDGE_CRC_INIT;
+    uint16_t crc = SPI_BRIDGE_CRC_INIT;
 
     crc = spi_bridge_crc_update(crc, raw[0]);
-    for (uint8_t i = 0U; i < length; ++i)
-    {
+    for (uint8_t i = 0U; i < length; ++i) {
         crc = spi_bridge_crc_update(crc, raw[1U + i]);
     }
 
     return crc;
 }
 
-static bool spi_bridge_start_transaction(void)
-{
+static bool spi_bridge_start_transaction(void) {
     xSemaphoreTake(xSPI_Semaphore, portMAX_DELAY);
-    if (spi_start_transfer(_SPI_MODE_0, CS_NO_TOGGLE, CS_RT1064) != SPI_OK)
-    {
+    if (spi_start_transfer(_SPI_MODE_0, CS_NO_TOGGLE, CS_RT1064) != SPI_OK) {
         xSemaphoreGive(xSPI_Semaphore);
         return false;
     }
     return true;
 }
 
-static void spi_bridge_end_transaction(void)
-{
+static void spi_bridge_end_transaction(void) {
     (void)spi_end_transfer();
     xSemaphoreGive(xSPI_Semaphore);
 }
 
-static bool spi_bridge_read_header(uint8_t en, uint8_t *header_out)
-{
-    if (header_out == NULL)
-    {
+static bool spi_bridge_read_header(uint8_t en, uint8_t* header_out) {
+    if (header_out == NULL) {
         return false;
     }
 
-    if (!spi_bridge_start_transaction())
-    {
+    if (!spi_bridge_start_transaction()) {
         return false;
     }
 
-    uint8_t command = (uint8_t)((kSpiBridgeCommandReadHeader << SPI_BRIDGE_CMD_OP_SHIFT) |
-                                (en & SPI_BRIDGE_CMD_EN_MASK));
+    uint8_t command =
+        (uint8_t)((kSpiBridgeCommandReadHeader << SPI_BRIDGE_CMD_OP_SHIFT) |
+                  (en & SPI_BRIDGE_CMD_EN_MASK));
     (void)spi_partial_transfer(&command);
 
     uint8_t header = 0U;
@@ -129,139 +114,136 @@ static bool spi_bridge_read_header(uint8_t en, uint8_t *header_out)
     return true;
 }
 
-static bool spi_bridge_read_block(uint8_t en, spi_bridge_block_t *block)
-{
-    if (block == NULL)
-    {
+static bool spi_bridge_read_block(uint8_t en, spi_bridge_block_t* block) {
+    if (block == NULL) {
         return false;
     }
 
-    if (!spi_bridge_start_transaction())
-    {
+    if (!spi_bridge_start_transaction()) {
         return false;
     }
 
-    uint8_t command = (uint8_t)((kSpiBridgeCommandReadBlock << SPI_BRIDGE_CMD_OP_SHIFT) |
-                                (en & SPI_BRIDGE_CMD_EN_MASK));
+    uint8_t command =
+        (uint8_t)((kSpiBridgeCommandReadBlock << SPI_BRIDGE_CMD_OP_SHIFT) |
+                  (en & SPI_BRIDGE_CMD_EN_MASK));
     (void)spi_partial_transfer(&command);
 
-    uint8_t raw[SPI_BRIDGE_BLOCK_SIZE] = { 0 };
+    uint8_t raw[SPI_BRIDGE_BLOCK_SIZE] = {0};
     (void)spi_partial_transfer(&raw[0]);
 
     uint8_t length = spi_bridge_extract_length(raw[0]);
-    if (length > SPI_BRIDGE_MAX_PAYLOAD_LENGTH)
-    {
+    if (length > SPI_BRIDGE_MAX_PAYLOAD_LENGTH) {
         spi_bridge_end_transaction();
         return false;
     }
 
     uint8_t total = (uint8_t)(length + 2U);
-    for (uint8_t i = 1U; i <= total; ++i)
-    {
+    for (uint8_t i = 1U; i <= total; ++i) {
         (void)spi_partial_transfer(&raw[i]);
     }
 
     spi_bridge_end_transaction();
 
     block->header = raw[0];
-    if (length > 0U)
-    {
+    if (length > 0U) {
         (void)memcpy(block->payload, &raw[1], length);
     }
-    if (length < SPI_BRIDGE_MAX_PAYLOAD_LENGTH)
-    {
-        (void)memset(&block->payload[length], 0, SPI_BRIDGE_MAX_PAYLOAD_LENGTH - length);
+    if (length < SPI_BRIDGE_MAX_PAYLOAD_LENGTH) {
+        (void)memset(&block->payload[length], 0,
+                     SPI_BRIDGE_MAX_PAYLOAD_LENGTH - length);
     }
-    block->crc = (uint16_t)(raw[1U + length] | ((uint16_t)raw[2U + length] << 8U));
+    block->crc =
+        (uint16_t)(raw[1U + length] | ((uint16_t)raw[2U + length] << 8U));
 
     return (spi_bridge_compute_crc(raw) == block->crc);
 }
 
-static void spi_bridge_update_endpoint_state(const spi_bridge_block_t *hub_block)
-{
-    if (hub_block == NULL)
-    {
+static void spi_bridge_update_endpoint_state(
+    const spi_bridge_block_t* hub_block) {
+    if (hub_block == NULL) {
         return;
     }
 
     uint8_t length = spi_bridge_extract_length(hub_block->header);
-    if (length < (SPI_BRIDGE_MAX_DEVICES + 1U))
-    {
+    if (length < (SPI_BRIDGE_MAX_DEVICES + 1U)) {
         return;
     }
 
     bool changed = false;
-    for (uint8_t i = 0U; i < SPI_BRIDGE_MAX_DEVICES + 1U; ++i)
-    {
+    for (uint8_t i = 0U; i < SPI_BRIDGE_MAX_DEVICES + 1U; ++i) {
         uint8_t new_value = hub_block->payload[i];
-        if (s_connected_table[i] != new_value)
-        {
+        if (s_connected_table[i] != new_value) {
             s_connected_table[i] = new_value;
             changed = true;
         }
     }
 
-    if (changed)
-    {
-        for (uint8_t i = 0U; i < SPI_BRIDGE_MAX_DEVICES; ++i)
-        {
+    if (changed) {
+        for (uint8_t i = 0U; i < SPI_BRIDGE_MAX_DEVICES; ++i) {
             s_endpoint_active[i + 1U] = (s_connected_table[i] != 0U);
         }
         s_endpoint_active[SPI_BRIDGE_CDC_ENDPOINT_INDEX] =
             (s_connected_table[SPI_BRIDGE_MAX_DEVICES] != 0U);
 
-        debug_print("SPI bridge: hub status update (slots=%u%u%u%u, cdc=%u)\r\n",
-                    s_endpoint_active[1U], s_endpoint_active[2U], s_endpoint_active[3U],
-                    s_endpoint_active[4U], s_endpoint_active[SPI_BRIDGE_CDC_ENDPOINT_INDEX]);
+        debug_print(
+            "SPI bridge: hub status update (slots=%u%u%u%u, cdc=%u)\r\n",
+            s_endpoint_active[1U], s_endpoint_active[2U], s_endpoint_active[3U],
+            s_endpoint_active[4U],
+            s_endpoint_active[SPI_BRIDGE_CDC_ENDPOINT_INDEX]);
     }
 }
 
-static void spi_bridge_poll_endpoints(void)
-{
-    for (uint8_t en = 1U; en < SPI_BRIDGE_MAX_LOGICAL_ENDPOINTS; ++en)
-    {
-        if (!s_endpoint_active[en])
-        {
+static void spi_bridge_poll_endpoints(void) {
+    for (uint8_t en = 1U; en < SPI_BRIDGE_MAX_LOGICAL_ENDPOINTS; ++en) {
+        if (!s_endpoint_active[en]) {
             continue;
         }
 
         uint8_t header = 0U;
-        if (!spi_bridge_read_header(en, &header))
-        {
+        if (!spi_bridge_read_header(en, &header)) {
             continue;
         }
 
-        if ((header & SPI_BRIDGE_HEADER_DIRTY_MASK) == 0U)
-        {
+        if ((header & SPI_BRIDGE_HEADER_DIRTY_MASK) == 0U) {
             continue;
         }
 
         spi_bridge_block_t block;
-        if (!spi_bridge_read_block(en, &block))
-        {
+        if (!spi_bridge_read_block(en, &block)) {
             debug_print("SPI bridge: CRC mismatch on EN %u\r\n", en);
             continue;
         }
 
         uint8_t length = spi_bridge_extract_length(block.header);
-        uint8_t type   = (block.header & SPI_BRIDGE_HEADER_TYPE_MASK) ? 1U : 0U;
+        uint8_t type = (block.header & SPI_BRIDGE_HEADER_TYPE_MASK) ? 1U : 0U;
 
-        if (en == SPI_BRIDGE_CDC_ENDPOINT_INDEX)
-        {
+        if (en == SPI_BRIDGE_CDC_ENDPOINT_INDEX) {
             s_cdc_in_block = block;
-            debug_print("SPI bridge: CDC IN (%u bytes, type=%u)\r\n", length, type);
-        }
-        else
-        {
+            debug_print("SPI bridge: CDC IN (%u bytes, type=%u)\r\n", length,
+                        type);
+        } else {
             uint8_t slot = (uint8_t)(en - 1U);
             s_in_blocks[slot] = block;
-            debug_print("SPI bridge: HID IN slot %u (%u bytes, type=%u)\r\n", slot, length, type);
+            debug_print("SPI bridge: HID IN slot %u (%u bytes, type=%u)\r\n",
+                        slot, length, type);
         }
     }
 }
 
-static void task_spi_bridge(void *pvParameters)
-{
+static void task_spi_bridge(void* pvParameters) {
+    /* Set the PCK0 clock source to MAIN_CLK and clock divider to 0*/
+    ((Pmc*)PMC)->PMC_PCK[0] = 1;
+
+    /* Enable PCK0 output to pin*/
+    ((Pmc*)PMC)->PMC_SCER |= PMC_SCER_PCK0;
+
+    /* Setup the clock pin from the processor to CPLD to mode MUX D*/
+    ((Pio*)PIOB)->PIO_ABCDSR[0] |= PIO_PB12;
+    ((Pio*)PIOB)->PIO_ABCDSR[1] |= PIO_PB12;
+    /*Disables the PIO from controlling the corresponding pin (enables
+     * peripheral control of the pin).*/
+    ((Pio*)PIOB)->PIO_PDR = PIO_PB12;
+
     (void)pvParameters;
 
     (void)memset(&s_hub_status, 0, sizeof(s_hub_status));
@@ -271,21 +253,15 @@ static void task_spi_bridge(void *pvParameters)
     (void)memset(s_endpoint_active, 0, sizeof(s_endpoint_active));
     s_endpoint_active[SPI_BRIDGE_CDC_ENDPOINT_INDEX] = true;
 
-    for (;;)
-    {
+    for (;;) {
         uint8_t header = 0U;
-        if (spi_bridge_read_header(0U, &header))
-        {
-            if ((header & SPI_BRIDGE_HEADER_DIRTY_MASK) != 0U)
-            {
+        if (spi_bridge_read_header(0U, &header)) {
+            if ((header & SPI_BRIDGE_HEADER_DIRTY_MASK) != 0U) {
                 spi_bridge_block_t hub_block;
-                if (spi_bridge_read_block(0U, &hub_block))
-                {
+                if (spi_bridge_read_block(0U, &hub_block)) {
                     s_hub_status = hub_block;
                     spi_bridge_update_endpoint_state(&hub_block);
-                }
-                else
-                {
+                } else {
                     debug_print("SPI bridge: hub CRC mismatch\r\n");
                 }
             }
@@ -296,11 +272,9 @@ static void task_spi_bridge(void *pvParameters)
     }
 }
 
-void rt1064_spi_bridge_init(void)
-{
-    if (xTaskCreate(task_spi_bridge, "SPI Bridge", TASK_SPI_BRIDGE_STACK_SIZE, NULL,
-                    TASK_SPI_BRIDGE_STACK_PRIORITY, NULL) != pdPASS)
-    {
+void rt1064_spi_bridge_init(void) {
+    if (xTaskCreate(task_spi_bridge, "SPI Bridge", TASK_SPI_BRIDGE_STACK_SIZE,
+                    NULL, TASK_SPI_BRIDGE_STACK_PRIORITY, NULL) != pdPASS) {
         debug_print("Failed to create SPI bridge task\r\n");
     }
 }
